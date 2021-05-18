35a812e35ae77aefef2b142dc0905bce3aca8858
Juan Ernesto Garcia Alvarez <jegasmlm@gmail.com>
2021-05-18 23:02:12 +0200

    add magin to tournament card

--- src/components/elements/PlayerItem.js
+++ src/components/elements/PlayerItem.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import Avatar from './Avatar'
 
-function PlayerItem({player, horizontal, size, className, onClick, props}) {
+function PlayerItem({player, horizontal, size, className, onClick, style, props}) {
   return (
-    <div className={className + (horizontal ? ' h-layout align-center' : ' v-layout')} key={player.id} onClick={onClick} {...props}>
+    <div className={className + (horizontal ? ' h-layout align-center' : ' v-layout')} key={player.id} onClick={onClick} style={style} {...props}>
       <Avatar big={size === 'lg'} url={player.avatar || null}/>
       <span className={`${size === 'sm' ? 'text-sm' : size === 'xs' ? 'text-xs' : size === 'lg' ? 'text-lg' : ''}`} style={{marginLeft: horizontal ? 4 : 0, marginTop: horizontal ? 0 : 4}}>{player.name || player}</span>
     </div>
