25ae6ba3dd04d2933e29477e09631a33f5202e83
Juan Ernesto Garcia Alvarez <jegasmlm@gmail.com>
2021-05-20 00:31:17 +0200

    Fix player item long names

--- src/components/views/Results/Position.js
+++ src/components/views/Results/Position.js
@@ -21,7 +21,7 @@ function Position({playerScore, place}) {
 
   return (
     <div className={`place flex-grow ${place === 1 ? 'first-place' : place === 2 ? 'second-place' : 'third-place'} ${!isMobile ? 'mr ml ' : ''}`}>
-      <PlayerItem tooltip={isMobile} size="lg" player={playerScore.player} />
+      <PlayerItem tooltip={isMobile}  ellipsis size="lg" player={playerScore.player} />
       <div className='box v-layout'>
         <h1 className='text-white m0'>{place}<span className='text-sm'>{place === 1 ? 'st' : place === 2 ? 'nd' : 'rd'}</span></h1>
         <span className='text-sm mt'>{parseInt(playerScore.kills) + parseInt(playerScore.points)} Pts</span>
